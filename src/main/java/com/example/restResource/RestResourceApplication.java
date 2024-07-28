8eb8e7a1c6cdee5d22ef0677c29e7dc44e058a62
billybobozhu <zhubo970202@gmail.com>
2024-07-28 14:51:41 +0800

    Initial commit for form

---
+++ src/main/java/com/example/restResource/RestResourceApplication.java
@@ -0,0 +1,12 @@
+package com.example.restResource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestResourceApplication.class, args);
+    }
+
+}
